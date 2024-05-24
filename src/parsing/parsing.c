357369db80fd095921c7af94b5e016321ae89cde
Guillem Forns <132559287+GuillemFo@users.noreply.github.com>
2024-05-24 04:52:21 +0000

    adding notes' '

--- src/parsing/parsing.c
+++ src/parsing/parsing.c
@@ -6,8 +6,22 @@
 /*   By: codespace <codespace@student.42.fr>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/23 07:31:14 by codespace         #+#    #+#             */
-/*   Updated: 2024/05/23 09:17:20 by codespace        ###   ########.fr       */
+/*   Updated: 2024/05/24 04:51:41 by codespace        ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "cube3D.h"
+
+confirm .cub FILE and only 1 arg
+
+◦ The map must be closed/surrounded by walls, if not the program must return
+an error.
+◦ Except for the map content, each type of element can be separated by one or
+more empty line(s).
+◦ Except for the map content which always has to be the last, each type of
+element can be set in any order in the file.
+◦ Except for the map, each type of information from an element can be separated
+by one or more space(s).
+◦ The map must be parsed as it looks in the file. Spaces are a valid part of the
+map and are up to you to handle. You must be able to parse any kind of map,
+as long as it respects the rules of the map.
