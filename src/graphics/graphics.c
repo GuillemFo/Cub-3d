cab4f4aa5e6096c44ed22653dcd689e08cf8e120
Guillem Forns Sant <gforns-s@car9s4.42barcelona.com>
2024-06-18 08:20:14 +0200

    fixing ifs for linux

--- src/graphics/graphics.c
+++ src/graphics/graphics.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   graphics.c                                         :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: josegar2 <josegar2@student.42.fr>          +#+  +:+       +#+        */
+/*   By: gforns-s <gforns-s@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/12 15:43:09 by gforns-s          #+#    #+#             */
-/*   Updated: 2024/06/17 21:41:33 by josegar2         ###   ########.fr       */
+/*   Updated: 2024/06/18 08:19:00 by gforns-s         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -58,10 +58,14 @@ int img_init(t_data *data)
 {
 	data->mlx->img = ft_calloc(1, sizeof(t_image));
     if (!data->mlx->img)
+	{
         return (1);
+	}
 	data->mlx->img->img = mlx_new_image(data->mlx->mlx, WIN_X, WIN_Y);
     if (!data->mlx->img->img)
+	{
         return (1);
+	}
 	data->mlx->img->addr = mlx_get_data_addr(data->mlx->mlx, &data->mlx->img->bpp, &data->mlx->img->ll, &data->mlx->img->en);
 	return (0);
 }
@@ -71,11 +75,15 @@ int	start_mlx(t_data *data)
 {
 	data->mlx->mlx = mlx_init();
     if (!data->mlx->mlx)
+	{
         return (message("mlx initialitation error\n"), 1);
+	}
 	data->mlx->win = mlx_new_window(data->mlx->mlx, WIN_X, WIN_Y,
 			"Cub3D gforns-s & josegar2");
     if (load_textures(data->file, data->mlx))
-        return (1);
+	{
+		return (1);
+	}
 	if (img_init(data))
         return (message("Image creation error\n"), 1);
 	// Been told to start first with a color innstead of image
