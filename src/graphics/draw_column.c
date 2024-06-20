2075aba84443c4f3968d16767c2c33c1948facd9
josegar2 <josegar2@student.42barcelona.com>
2024-06-20 16:19:44 +0200

    1st draw texture with many args and a weird behaviour

--- src/graphics/draw_column.c
+++ src/graphics/draw_column.c
@@ -6,7 +6,7 @@
 /*   By: josegar2 <josegar2@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/18 12:00:29 by josegar2          #+#    #+#             */
-/*   Updated: 2024/06/19 22:07:51 by josegar2         ###   ########.fr       */
+/*   Updated: 2024/06/20 15:56:50 by josegar2         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,8 +20,34 @@ void	c3d_mlx_pixel_put(t_image im, int x, int y, int color)
 	*(unsigned int *)dst = color;
 }
 
+int get_texture_color(t_image im, int x, int y)
+{
+	char *dst;
+
+	dst = im.addr + (y * im.ll + x * (im.bpp / 8));
+	return (*(unsigned int *)dst);
+}
+
+void	draw_texture(t_graph *g, int x, int sow, int side, int sidex, int off)
+{
+	int 	y;
+	float	yratio;
+	int		txty;
+	int		color;
+
+	(void)off;
+	y = (WIN_Y - sow) / 2;
+	yratio = g->txt[side].h / sow;
+	txty = 0;
+	while (y > 0 && y < WIN_Y && y < sow + (WIN_Y - sow) / 2)
+	{
+		color = get_texture_color(g->txt[side], sidex, txty++ * yratio);
+		c3d_mlx_pixel_put(g->i, x, y++, color);
+	}
+}
+
 // draw the column x of the scene, with a SizeOfWall sow and an offset off
-// off == 0 means
+// off == 0 means in the center
 void	draw_column(t_graph *g, int x, int sow, int off)
 {
 	int y;
@@ -32,10 +58,7 @@ void	draw_column(t_graph *g, int x, int sow, int off)
 	{
 		c3d_mlx_pixel_put(g->i, x, y++, g->rgbc);
 	}
-	while (y < WIN_Y && y < sow + (WIN_Y - sow) / 2)
-	{
-		c3d_mlx_pixel_put(g->i, x, y++, 0x00FFFFFF);
-	}
+    y += sow;
 	while (y < WIN_Y)
 	{
 		c3d_mlx_pixel_put(g->i, x, y++, g->rgbf);
@@ -46,12 +69,18 @@ void	check_columns(t_graph *g)
 {
 	int	x;
 	int	sow;
+    int sidex;
 
 	x = 0;
+    sidex = 0;
 	while (x < WIN_X)
 	{
 		sow = x % WIN_Y;
+		if (!sow)
+			sow++;
 		draw_column(g, x, sow, 0);
+        draw_texture(g, x, sow, 0, sidex, 0);
+        sidex = (sidex + 1) % BLOCK_SIZE;
 		x++;
 	}
 	mlx_put_image_to_window(g->mlx, g->win, g->i.img, 0, 0);
